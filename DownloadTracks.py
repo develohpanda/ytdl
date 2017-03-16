d8ae9fa4d8cd888f45b02c6289e92e001d6bdf28
Opender Singh <opender94@gmail.com>
2017-03-17 03:11:47 +1300

    added downloaded tracks.py

---
+++ DownloadTracks.py
@@ -0,0 +1,28 @@
+"""The document"""
+
+from gmusicapi import Musicmanager
+
+def run():
+    """Downloads the stuff."""
+
+    manager = Musicmanager()
+    manager.login(
+        '..\\gmusicapi-musicmanager.cred'
+        , '09:32:24:12:CD:AA'
+        , 'ytdl-bot')
+
+    upload_result = manager.upload(
+        "E:\\Music\\Good Riddance, Heathens (feat. twent.mp3"
+        , True
+        , True)
+
+    if upload_result[0] != {}:
+        print "Uploaded " + upload_result[0]
+    elif upload_result[1] != {}:
+        print "Uploaded and matched " + upload_result[1]
+    elif upload_result[2] != {}:
+        reason = list(upload_result[2].viewitems())[0]
+        print "Couldn't upload " + reason[0] + " because " + reason[1]
+
+if __name__ is '__main__':
+    run()
