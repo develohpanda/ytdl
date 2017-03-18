09562cc853f851b927a3f040d4249fe6bf4872ad
Opender Singh <opender94@gmail.com>
2017-03-19 04:35:53 +1300

    Update

--- gmupload.py
+++ gmupload.py
@@ -3,11 +3,6 @@
 import oshelper
 from gmusicapi import Musicmanager
 
-DEFAULT_FILE_NAME = ''
-
-def __get_track_file__(files):
-    return next((f for f in files if f.endswith('.mp3')), DEFAULT_FILE_NAME)
-
 def __upload_file__(track_file):
     manager = Musicmanager()
     manager.login(
@@ -19,24 +14,21 @@ def __upload_file__(track_file):
 
     if upload_result[0] != {}:
         print "Uploaded " + upload_result[0]
-        return upload_result[0]
 
     elif upload_result[1] != {}:
         print "Uploaded and matched " + upload_result[1]
-        return upload_result[1]
 
     elif upload_result[2] != {}:
         reason = list(upload_result[2].viewitems())[0]
         print "Couldn't upload " + reason[0] + " because " + reason[1]
-        return ''
 
 def upload(trackdir):
     "Does the upload and returns the track id if successfully uploaded."
 
     files = oshelper.absolute_files(trackdir)
 
-    track_file = __get_track_file__(files)
-    if track_file == DEFAULT_FILE_NAME:
+    track_file = oshelper.get_track_file(files)
+    if track_file == oshelper.DEFAULT_FILE_NAME:
         return
 
-    uploaded_track_id = __upload_file__(track_file)
+    __upload_file__(track_file)
--- oshelper.py
+++ oshelper.py
@@ -15,3 +15,13 @@ def absolute_dirs(path):
         dirs = [os.path.join(path, f) for f in os.listdir(path)]
         return [f for f in dirs if os.path.isdir(f)]
     return []
+
+DEFAULT_FILE_NAME = ''
+
+def get_track_file(files):
+    "Gets the mp3 file from this folder"
+    return next((f for f in files if f.endswith('.mp3')), DEFAULT_FILE_NAME)
+
+def get_album_art_file(files):
+    "Gets the album art file from this folder"
+    return next((f for f in files if f.endswith('.jpg')), DEFAULT_FILE_NAME)
