88143d54c5955cc0fb6d551ce033338852d11e2e
Opender Singh <opender94@gmail.com>
2017-03-26 13:40:56 +1300

    Logging to console and file works

--- ytdl/main.py
+++ ytdl/main.py
@@ -7,16 +7,26 @@ from ytdl import Ytdl
 
 def main():
     "Main"
+    logging.getLogger('').handlers = []
+
     #oshelper.mkdir(oshelper.dirname(logs_file_path))
     logging.basicConfig(
         filename=os.path.join(
             'c:\\users\\opend\\ytdl\\logs',
             str(datetime.date.today()) + ".log"),
         level=logging.DEBUG,
-        format='%(asctime)s %(message)s',
+        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
         datefmt='%m/%d/%Y %I:%M:%S %p')
 
-    logging.debug("test")
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger('').addHandler(console)
 
     Ytdl().run()
 
