3431d5ce9ebd6ede9c194b2b5120e352dc9d2df2
Opender Singh <opender94@gmail.com>
2017-03-26 15:57:07 +1300

    Removing log files older than 7 days

--- ytdl/main.py
+++ ytdl/main.py
@@ -1,6 +1,7 @@
 "Main"
 
 import datetime
+import time
 import logging
 import os
 
@@ -13,10 +14,10 @@ def main():
     logging.getLogger('').handlers = []
     logs_file_path = 'c:\\users\\opend\\ytdl\\logs'
     oshelper.mkdir(logs_file_path)
-    file_name = os.path.join(logs_file_path, str(datetime.date.today()) + ".log")
+    logs_file_name = os.path.join(logs_file_path, str(datetime.date.today()) + ".log")
 
     logging.basicConfig(
-        filename=file_name,
+        filename=logs_file_name,
         level=logging.INFO,
         format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
         datefmt='%m/%d/%Y %I:%M:%S %p')
@@ -32,6 +33,14 @@ def main():
     logging.getLogger('').addHandler(console)
 
     Ytdl().run()
+
+    current_time = time.time()
+
+    for file_name in os.listdir(logs_file_path):
+        creation_time = os.path.getctime(file_name)
+        if (current_time - creation_time) // (24 * 3600) >= 7:
+            os.unlink(file_name)
+            logging.info('Removed %s', file_name)
 
 if __name__ == '__main__':
     main()
