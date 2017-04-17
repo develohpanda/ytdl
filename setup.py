d66e8f8477a58a37322e51f076fc15f5bc9ec79e
Opender Singh <opender94@gmail.com>
2017-04-18 02:30:10 +1200

    'V1.0.4'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='ytdl',
-    version='1.0.3',
+    version='1.0.4',
     packages=['ytdl'],
     url='https://github.com/develohpanda/ytdl',
     description='YT download and upload to GMusic',
